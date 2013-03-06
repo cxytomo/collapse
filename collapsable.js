970f849c42de4219b04aa7bc5068175a9b27650b
tomo <cxytomo@gmail.com>
2013-03-06 12:38:27 +0800

    collapsable
    @dribbble

---
+++ collapsable.js
@@ -0,0 +1,15 @@
+$('select').mouseenter(function (e) {
+	expand();
+});
+
+$('list').mouseleave(function (e) {
+	collapse();
+});
+
+var expand = function () {
+
+};
+
+var collapse = function(e) {
+	
+};
